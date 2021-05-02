32f7b4d2570abd4b1bd1a376df7219bb1610304a
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-05-01 21:20:50 -0300

    Static Side Generation.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 yarn dev, localhost:3000
 yarn server, "server": "json-server server.json -w -d 750 -p 3333"
+yarn start, inicia em modo de produção
 
 React
 Next.js
--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,5 +1,24 @@
-export default function Home() {
+// SSR
+// SSG
+
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+// getServerSideProps() função que será executada pelo next antes do conteúdo ser exibido na tela, será executado a cada requisição, Server Side Rendering
+export async function getStaticProps() { // Static Side Generation, gera uma página estático para cada usuário que acessar a aplicação que não precisa de atualizações frequentes. Só funciona em produção
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8, // 8 horas, tempo em segundos em que uma nova página será gerada
+  }
 }
