fec51d9ba5003294c187afb2610320655b8edeb6
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-05-12 22:49:07 -0300

    Pause e play com Refs.

--- podcastr/src/components/Player/index.tsx
+++ podcastr/src/components/Player/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Slider from 'rc-slider'
 
@@ -9,7 +9,26 @@ import { PlayerContext } from '../../contexts/PlayerContext'
 import styles from './styles.module.scss'
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+  const audioRef = useRef<HTMLAudioElement>(null) // cria referência da tag audio para executar funções de javascript puro no React
+
+  const { 
+    episodeList, 
+    currentEpisodeIndex, 
+    isPlaying, 
+    togglePlay 
+  } = useContext(PlayerContext)
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      return
+    }
+
+    if (isPlaying) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+  }, [isPlaying])
 
   const episode = episodeList[currentEpisodeIndex]
 
@@ -56,7 +75,11 @@ export function Player() {
         </div>
 
         { episode && (
-          <audio src={episode.url} autoPlay />
+          <audio 
+            src={episode.url} 
+            ref={audioRef}
+            autoPlay 
+          />
         )}
 
         <div className={styles.buttons}>
@@ -66,8 +89,16 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton} disabled={!episode}>
-            <img src="/play.svg" alt="Tocar" />
+          <button 
+            type="button" 
+            className={styles.playButton} 
+            disabled={!episode}
+            onClick={togglePlay}
+          >
+            { isPlaying
+              ? <img src="/pause.svg" alt="Pausar" />
+              : <img src="/play.svg" alt="Tocar" />
+            }
           </button>
           <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
--- podcastr/src/contexts/PlayerContext.ts
+++ podcastr/src/contexts/PlayerContext.ts
@@ -11,7 +11,9 @@ type Episode = {
 type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: Episode) => void;
+  togglePlay: () => void;
 }
 
 export const PlayerContext = createContext({} as PlayerContextData) // createContext é mais utilizado para definir o tipo de dado que será salvo no contexto
