b85c628e39a222bae9c6f9033f0aedf27135c3ce
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-05-16 18:50:28 -0300

    Refatorando contexto e tocar episódio específico.

--- podcastr/src/components/Player/index.tsx
+++ podcastr/src/components/Player/index.tsx
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Slider from 'rc-slider'
 
 import 'rc-slider/assets/index.css'
 
-import { PlayerContext } from '../../contexts/PlayerContext'
+import { usePlayer } from '../../contexts/PlayerContext'
 
 import styles from './styles.module.scss'
 
@@ -18,8 +18,10 @@ export function Player() {
     togglePlay,
     setPlayingState,
     playNext,
-    playPrevious
-  } = useContext(PlayerContext)
+    playPrevious,
+    hasNext,
+    hasPrevious
+  } = usePlayer()
 
   useEffect(() => {
     if (!audioRef.current) {
@@ -91,7 +93,7 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button" onClick={playPrevious} disabled={!episode}>
+          <button type="button" onClick={playPrevious} disabled={!episode || !hasPrevious}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
           <button 
@@ -105,7 +107,7 @@ export function Player() {
               : <img src="/play.svg" alt="Tocar" />
             }
           </button>
-          <button type="button" onClick={playNext} disabled={!episode}>
+          <button type="button" onClick={playNext} disabled={!episode || !hasNext}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
           <button type="button" disabled={!episode}>
